9e8a0a38c382c24182e4d026aac27a728ec65779
HoffmannMi1971 <122547457+HoffmannMi1971@users.noreply.github.com>
2023-01-12 16:38:38 +0100

    Add files via upload

---
+++ MSTRIO/Constants.py
@@ -0,0 +1,14 @@
+#SMTP Contants
+##############
+smtp_server = "smtp.ionos.de"
+port = 587  # For starttls
+sender_email = "xxx"
+sender_password = "xxx"
+
+#MSTR Environment Connection
+############################
+mstr_base_url = 'http://desktop-ivjma0m:8080/MicroStrategyLibrary/api'
+mstr_username = 'mh'
+mstr_password = ''
+mstr_project_id = 'B7CA92F04B9FAE8D941C3E9B7E0CD754'
+mstr_project_name = 'MicroStrategy Tutorial'
